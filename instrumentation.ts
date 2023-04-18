73c7d909536e28c9c6bdf8ad0c453a6b2efc8d42
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-04-18 16:59:26 +0530

    test vercel & next

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,14 +3,14 @@ import { track } from '@middleware.io/nextjs-vercel';
 
 export function register() {
     console.log("process.env.NEXT_RUNTIME==>",process.env.NEXT_RUNTIME);
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        track({
-            projectName: 'My-Third-Project16',
-            serviceName: 'My-Third-Service16',
-            accountKey: 'rwtnqhyqjwypwfkrqbkrpmijoydhyghzoctk',
-            target: 'https://yzxwv.stage.env.middleware.io:443'
-        });
-    }
+    // if (process.env.NEXT_RUNTIME === 'nodejs') {
+    track({
+        projectName: 'My-Third-Project16',
+        serviceName: 'My-Third-Service16',
+        accountKey: 'rwtnqhyqjwypwfkrqbkrpmijoydhyghzoctk',
+        target: 'https://yzxwv.stage.env.middleware.io:443'
+    });
+    // }
 
     /*if (process.env.NEXT_RUNTIME === 'edge') {
         track({
