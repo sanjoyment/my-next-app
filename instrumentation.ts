7239719d905bf587f239ae2e685459ecc3dc1e8e
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-04-18 12:36:50 +0530

    test vercel & next

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,17 @@
-// instrumentation.ts
-import { registerOTel } from '@vercel/otel'
+// @ts-ignore
+import { track } from '@middleware.io/nextjs-vercel';
 
 export function register() {
-    registerOTel('my-next-app')
+    track({
+        projectName: 'My-Third-Project',
+        serviceName: 'My-Third-Service',
+    });
+
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+        console.log("Node file called...");
+    }
+
+    if (process.env.NEXT_RUNTIME === 'edge') {
+        console.log("Edge file called...");
+    }
 }
