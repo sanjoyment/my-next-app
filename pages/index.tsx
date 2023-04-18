d961e62f7b43e91d7cdea76ebb83edb9cade97be
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-04-18 14:08:44 +0530

    test vercel & next

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  console.log("index.js called.");
+  console.log("index.js called.", process.env.NEXT_RUNTIME);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
