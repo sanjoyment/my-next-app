7a830bf2ac79a42f42fe28f08231b078f6d3110d
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-04-18 14:23:14 +0530

    test vercel & next

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   experimental: {
     instrumentationHook: true,
-    appDir: true,
+    // appDir: true,
   }
 }
 
